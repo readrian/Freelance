b24fad64d45fe991fde781ed8648ea2ee67fc1df
Adrian Rendl <arendl1405@gmail.com>
2021-06-01 19:49:15 +0200

    Hamburger working

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('nav-ul');
 
 hamburger.addEventListener('click', () => {
+
+  hamburger.querySelectorAll("span").forEach((span) => span.classList.toggle("open"));
   navUL.classList.toggle('show');
-})
+
+});
